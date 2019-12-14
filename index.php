853a85310e4513743a17e6a1637d3648dab212aa
Saimadhav Jonnada <30337778+busyfriend@users.noreply.github.com>
2019-12-14 11:14:03 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,208 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
+<link rel="stylesheet" type="text/css" href="designs1.css">
+<link rel="icon" href="IIIT-K.png" type="image/x-icon">
+  <title>IIIT Kottayam</title>
+</head>
+<body>
+<div class="header-content">
+<a href="https://www.facebook.com/IIITKtym/" target="_new"><img src="002-facebook.png" class="fb" title="IIIT-K Official Facebook page"></a>
+<a href="https://twitter.com/IIITKtym" target="_new"><img src="twitter.png" class="twitter" title="IIIT-K Official Twitter page"></a>
+<img src="IIIT-K-logo.jpg" class="IIIT-logo" alt="IIIT-K-logo">
+<div class="hindi-script">भारतीय&nbsp; सूचना&nbsp; प्रौध्योगिकी &nbsp;संस्थान, कोट्टायम</div>
+<h1>Indian Institute of Information Technology, Kottayam</h1>
+<ul class="topnav" id="myTopnav">
+  <li><a href="#home">Home</a></li>
+  <li><a href="#about">About IIITK</a></li>
+  <li class="drop-faculty">
+    <a href="javascript:openNav1()" class="btn-faculty">Faculty</a>
+    <div class="content-faculty" id="mySidenav1">
+      <a href="#">Dean</a>
+      <a href="#a">Faculty</a>
+      <a href="#">Technical</a>
+    </div>
+  </li>
+  <li><a href="#academics">Academics</a></li>
+  <li class="drop-facilities">
+    <a href="javascript:openNav2()" class="btn-facilities">Facilities</a>
+    <div class="content-facilities" id="mySidenav2">
+      <a href="#">Library</a>
+      <a href="#">Internet</a>
+      <a href="#">Hostel</a>
+    </div>
+  </li>
+  
+  <li class="drop-events">
+    <a href="javascript:openNav3()" class="btn-events">Events</a>
+    <div class="content-events" id="mySidenav3">
+      <a href="#">Link 1</a>
+      <a href="#">Link 2</a>
+      <a href="#">Link 3</a>
+    </div>
+  </li>
+  <li><a href="#contactus">Contact Us</a></li>
+  <li class="more"><a href="#more">More</a></li>
+  <li class="icon"><a href="javascript:void(0);" onclick="myFunction()" style="font-size: 17px;">&#9776;</a></li>
+</ul>
+</div>
+<div class="body-content">
+<div class="slideshow-container">
+<div class="mySlides fade">
+  <img src="merged.jpg">
+</div>
+
+<div class="mySlides fade">
+  <img src="merged1.jpg">
+</div>
+
+<div class="mySlides fade">
+  <img src="merged2.jpg">
+</div>
+
+<div class="mySlides fade">
+  <img src="merged3.jpg">
+</div>
+
+<div class="mySlides fade">
+  <img src="merged4.jpg">
+</div>
+</div>
+<div style="text-align:center">
+  <span class="dot" onclick="currentSlide(1)"></span> 
+  <span class="dot" onclick="currentSlide(2)"></span> 
+  <span class="dot" onclick="currentSlide(3)"></span> 
+  <span class="dot" onclick="currentSlide(4)"></span>
+  <span class="dot" onclick="currentSlide(5)"></span> 
+</div>
+<div class="box-2 box-property">
+<img src="news.png" style="width:100px;height: 100px;  float: right;">
+<h2 style="padding-left: 25px;">News</h2><hr>
+<marquee direction="up" scrolldelay="220" style="height: 200px;">
+<ul onmouseover="this.parentNode.stop();" onmouseout="this.parentNode.start();">
+<li><a href="iiitpdf/permanent_campus_progress.pdf">IIITK's permanent campus - work in progress</a></li>
+<li><a href="http://www.iiitkottayam.ac.in/yoga.php" target="_new">International Yoga Day Celebrations-2017</a></li>
+<li><a href="iiitpdf/2017admission.pdf" target="_new">IIITK fee structure and academic</a></li>
+<li><a href="iiitpdf/scan.PDF" target="_new">Rank list published for the recruitment of various faculty positions</a></li>
+<li><a href="http://www.ndl.iitkgp.ac.in" target="_new">National Digital Library of India</a></li>
+<li><a href="http://117.232.110.174/moodle" target="_new">Virtual Learning Platform-IIITK</a></li>
+</ul>
+</marquee>
+</div>
+<div class="box-1 box-property">
+<img src="gre.png" style="float: left;padding-right: 10px;">
+<h2>Welcome to IIIT Kottayam</h2><hr>
+<p>The Ministry of Human Resources Development, Government of India has put forward a scheme wherein 20 new IIITs are proposed to be established in the country. A major objective in establishing IIITs is to set up an education model which can produce best-in-class human resources in IT and which can harness the multidimensional facets of IT in various domains. The IIIT is expected to contribute significantly to the global competitiveness through the key sectors of Indian economy and Industry, focusing on applied research and education in IT in selected domain areas.</p>
+<p>Accordingly, setting up of a IIIT in Kottayam district under the 20-IIIT scheme of Government of India is sanctioned. The initial investment involved is 128 Crores, 64 Crores being grant from the Central Government, 45 crores grant from Government of Kerala and 19 crores raised through private participation. In addition, the state will provide necessary land free of cost. The IIIT shall be located at Pala in Kottayam District. The land acquired is located at Vallichira village, Block No.20, Kottayam District.</p>
+</div>
+<div class="box-3 box-property">
+<img src="vision-icon.png" style="width: 120px;height: 80px;  float: right;">
+  <h2 style="padding-left: 25px;">Vision</h2><hr>
+  <p>To contribute to India and the World through excellence in Information technology, technical education and research and to serve as a valuable resource for industry and society</p>
+</div>
+<div class="box-4 box-property">
+<img src="mis.png" style="width: 100px;height: 75px;padding-top: 20px;padding-right: 0px;  float: right;">
+  <h2 style="padding-left: 25px;">Mission</h2><hr>
+  <p>Generate a specialized cadre of Information technology expertise by imparting quality education, training and to attain international standards in teaching, research and consultancy with global linkages</p>
+</div>
+<div class="box-5 box-property">
+  <h2 style="padding-left: 25px;padding-top: 20px;">Publications</h2><hr>
+  <ul>
+
+                        <li>
+                            Sparse channel estimation in OFDM systems using compressed sensing techniques in a Bayesian framework
+                            <span class="author"> - Dr.Renu Jose</span>
+                        </li>
+
+                        <li><a href="http://www.sciencedirect.com/science/article/pii/S1568494617302296">
+            “Flocking based Evolutionary Computation Strategy for measuring Centrality of Online Social Networks”. Applied Soft computing, Elsevier</a>
+                            <span class="author"> - Dr.Ebin Deni raj</span>
+                        </li>
+                    </ul>
+</div>
+<div class="privateP">
+<div class="private-partners"><marquee>IIITK Private Partners</marquee></div>
+<div class="pp-imgs">
+<img src="rolta.jpg" class="rolta" title="Rolta India">
+<img src="lulu.jpg" class="lulu" title="Lulu Group International">
+<img src="cial.jpg" class="cial" title="Cochin International Airport Limited">
+</div>
+</div>
+<div class="end"><marquee>Body-content ends</marquee></div>
+</div>
+<footer>
+  &copy;2017 - IIIT Kottayam<br>
+  Indian Institute of Information Technology, Kottayam
+</footer>
+<div onclick="topFunction()" id="topScrollBtn" title="Go to top">&#x27a0;</div  >  
+<script>
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex> slides.length) {slideIndex = 1}    
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 3000); // Change image every 2 seconds
+}
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("topScrollBtn").style.display = "block";
+    } else {
+        document.getElementById("topScrollBtn").style.display = "none";
+    }
+}
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+function openNav1() {
+  var x = document.getElementsByClassName("topnav responsive content-faculty");
+    x.style.width = "40%";
+}
+function openNav2() {
+    document.getElementsByClassName("content-facilities").style.width = "40%";
+}
+function openNav3() {
+    document.getElementsByClassName("content-events").style.width = "40%";
+}
+function closeNav() {
+    document.getElementsByClassName("content-faculty").style.width = "0";
+    document.getElementsByClassName("content-facilities").style.width = "0";
+    document.getElementsByClassName("content-events").style.width = "0";
+}
+/*window.onclick = function(event) {
+  if (!event.target.matches('btn-faculty btn-facilities btn-events')) {
+   if (!event.target.matches('content-faculty content-facilities content-events')){
+   closeNav();
+   }
+  }
+}*/
+</script>
+</body>
+</html>
